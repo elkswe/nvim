c4ed6738fd9d2b7b786570cf130db258115aea1b
Egor Ananko <egorananko@gmail.com>
2025-11-16 21:40:56 +0300

    feat(plugins): gitsigns - attach to untracked files

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,6 +4,7 @@ return {
   'lewis6991/gitsigns.nvim',
   event = { 'BufReadPost', 'BufNewFile' },
   opts = {
+    attach_to_untracked = true,
     on_attach = function(bufnr)
       local gitsigns = require 'gitsigns'
 
